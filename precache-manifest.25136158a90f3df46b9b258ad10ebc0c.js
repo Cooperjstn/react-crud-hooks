1e652926a3cbd6329620223b0371320a461b8f93
J. Cooper <cooperjstn@gmail.com>
2019-09-04 22:39:02 -0400

    Updates

---
+++ precache-manifest.25136158a90f3df46b9b258ad10ebc0c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ec2c0b7da65dea7c8218eaa03606882",
+    "url": "/react-crud-hooks/index.html"
+  },
+  {
+    "revision": "f35c81cdafac5618e003",
+    "url": "/react-crud-hooks/static/css/2.667c67ac.chunk.css"
+  },
+  {
+    "revision": "0f1aac6219a607a46721",
+    "url": "/react-crud-hooks/static/css/main.8cf0a0d6.chunk.css"
+  },
+  {
+    "revision": "f35c81cdafac5618e003",
+    "url": "/react-crud-hooks/static/js/2.ac0f645d.chunk.js"
+  },
+  {
+    "revision": "0f1aac6219a607a46721",
+    "url": "/react-crud-hooks/static/js/main.4b7abcf6.chunk.js"
+  },
+  {
+    "revision": "b8513db300246aaf6532",
+    "url": "/react-crud-hooks/static/js/runtime~main.9dfef1b4.js"
+  },
+  {
+    "revision": "56b452e5d7b89195b76474af4899b4d8",
+    "url": "/react-crud-hooks/static/media/Stellar-Bold.56b452e5.woff2"
+  },
+  {
+    "revision": "86282d258556c170036727967f0c9c0c",
+    "url": "/react-crud-hooks/static/media/Stellar-Bold.86282d25.woff"
+  },
+  {
+    "revision": "7ed7fa51ccd52fb60c10cc7ad55ee9b5",
+    "url": "/react-crud-hooks/static/media/Stellar-Light.7ed7fa51.woff2"
+  },
+  {
+    "revision": "d51c270e6fa1da8f5f28e241aa88422f",
+    "url": "/react-crud-hooks/static/media/Stellar-Light.d51c270e.woff"
+  }
+]);
